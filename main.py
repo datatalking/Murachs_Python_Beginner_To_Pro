cb03b77be9f905f6d32d25baaea121b75a96bb3b
Andrew Schell <paintstone@gmail.com>
2021-09-20 15:15:44 -0700

    added TOC layout for main.py sections 1 to 4 plus index with todofix on env error

---
+++ main.py
@@ -0,0 +1,44 @@
+# Murachs python
+
+
+def section_one():
+	"""
+	chapter layout
+	:return:
+	"""
+
+
+def section_two():
+	"""
+	chapters
+	:return:
+	"""
+	
+
+def section_three():
+	"""
+	chapters
+	:return:
+	"""
+
+
+def section_four():
+	"""
+	chapters
+	:return:
+	"""
+
+
+def appendix_list():
+	"""
+	stuff
+	:return:
+	"""
+
+"""
+# TODOFix getting an enviroment error. will diagnose if it persissts. I disabled rqd.txt plugin.
+1:42 PM	PyCharm has failed to load the environment from '/bin/bash'. Integration with tools that rely on environment variables may work incorrectly.
+				Learn more   Don't show again
+
+1:43 PM	Plugin error: File '~/Library/Application Support/JetBrains/Toolbox/apps/PyCharm-P/ch-0/212.5284.44/PyCharm.app.plugins/EnvFile/lib/envfile-3.2.1.jar' contains invalid plugin descriptor
+"""
