bee5435881222c7c7288a65343b9d420199090b8
Andrew Schell <paintstone@gmail.com>
2021-09-22 09:55:58 -0700

    added code editor and source code functions

---
+++ Section_One/python_compiling_source.py
@@ -0,0 +1,58 @@
+# Murachs Python
+
+
+def code_editor():
+	"""
+	text_editor name
+	:return:
+	"""
+	text_editor = str(['Sublime', 'PyCharm', 'JupyterNotebook', 'IDLE'])  # TODO add random picker for format below
+	print("the programmer maybe uses " + text_editor + " or IDE to enter and edit the source code.").format(text_editor)
+
+def source_code():
+	"""
+	
+	:return:
+	"""
+	file_types = str(['.py', '.md', 'cfg'])
+	print(" Then the programmer saves the source code to a file with .py")  # TODO add format
+
+def python_interpreter():
+	pass
+
+
+def bytecode():
+	pass
+
+
+def python_virtual_machine():
+	pass
+
+
+def operating_system():
+	pass
+
+
+def main():
+	code_editor()
+	source_code()
+	python_interpreter()
+	bytecode()
+	python_virtual_machine()
+	operating_system()
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
