5c44ce46147f3cf95c19e8d281179c6884dfdf59
Andrew Schell <paintstone@gmail.com>
2021-09-27 05:57:11 -0700

    added chapter summary and a few lists of tests

---
+++ test_Murachs_Python.py
@@ -0,0 +1,76 @@
+# SOURCE https://www.softwaretestinghelp.com/types-of-software-testing/#1_Alpha_Testing
+# SOURCE https://realpython.com/python-testing/#automating-the-execution-of-your-tests
+# REFERENCE https://docs.python.org/3.7/library/unittest.html#test-discovery
+
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+	def test_something(self):
+		self.assertEqual(True, False)  # add assertion here
+
+
+class MoreTestCases(unittest.TestCase):
+	def test_the_testers(self):
+		self.assertEqual()
+
+
+class CryptoTestCase(unittest.TestCase):
+	def test_crypto_exists(self):
+		self.
+	
+
+
+"""
+#1) Alpha Testing
+#2) Acceptance Testing
+#3) Ad-hoc Testing
+#4) Accessibility Testing
+#5) Beta Testing
+#6) Back-end Testing
+#7) Browser Compatibility Testing
+#8) Backward Compatibility Testing
+#9) Black Box Testing
+#10) Boundary Value Testing
+#11) Branch Testing
+#12) Comparison Testing
+#13) Compatibility Testing
+#14) Component Testing
+#15) End-to-End Testing
+#16) Equivalence Partitioning
+#17) Example Testing
+#18) Exploratory Testing
+#20) Functional Testing
+#21) Graphical User Interface (GUI) Testing
+#22) Gorilla Testing
+#23) Happy Path Testing
+#24) Incremental Integration Testing
+#25) Install/Uninstall Testing
+#26) Integration Testing
+#27) Load Testing
+#28) Monkey Testing
+#29) Mutation Testing
+#30) Negative Testing
+#31) Non-Functional Testing
+#32) Performance Testing
+#33) Recovery Testing
+#34) Regression Testing
+#35) Risk-Based Testing (RBT)
+#36) Sanity Testing
+#37) Security Testing
+#38) Smoke Testing
+#39) Static Testing
+#40) Stress Testing
+#41) System Testing
+#42) Unit Testing
+#43) Usability Testing
+#44) Vulnerability Testing
+#45) Volume Testing
+#46) White Box Testing
+"""
+
+
+
+
+if __name__ == '__main__':
+	unittest.main()
